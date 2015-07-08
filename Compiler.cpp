0abfaa2e4261bab08a9734c173d0c4161c8ac302
Yongwen Zhuang <zyeoman@163.com>
2015-07-08 23:46:11 +0800

    添加split，trim函数

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,8 +1,16 @@
+/*
+Filename : Compiler.cpp
+Compiler : Visual Studio 2013
+Description : Trans MIPS to HEX
+Release : 7/10/2015
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-#define VERSION 0.1
+#define VERSION 0.2
 
 using namespace std;
 
@@ -13,9 +21,48 @@ void showHelp(){
     cout<<"    -v,--version  :  Show Version"<<endl;
 }
 
+string& trim(string &s){
+	if (s.empty()){
+		return s;
+	}
+
+	s.erase(0, s.find_first_not_of(" "));
+	s.erase(s.find_last_not_of(" ") + 1);
+	return s;
+}
+
+void split(string &s, string& delim, vector< string > *ret){
+	int last = 0;
+	int index = s.find_first_of(delim, last);
+	while (index != string::npos){
+		ret->push_back(s.substr(last, index - last));
+		last = index + 1;
+		index = s.find_first_of(delim, last);
+	}
+	if (index - last>0){
+		ret->push_back(s.substr(last, index - last));
+	}
+}
+
+string& cut(string &s,char flag){
+	if (s.empty()){
+		return s;
+	}
+
+	s.erase(0, s.find_first_of(flag));
+	return s;
+}
+
+int trans(vector<string>content, int i){
+	return 1;
+}
+
 int main(int argc, char const *argv[]){
     if (argc==1){
         showHelp();
+        ofstream outputFile("output.hex",ios::binary|ios::out);
+		char *a = "101100";
+        outputFile.write(a,sizeof(a));
     }
     else if(argc==2){
         string options=argv[1];
@@ -24,7 +71,7 @@ int main(int argc, char const *argv[]){
         }
         else if(options=="-v"||options=="--version"){
             cout<<"Copyright (C) Yeoman Zhuang. All rights reserved."<<endl;
-            cout<<"MIPS Compiler; from MIPS to HEX"<<endl;
+            cout<<"MIPS Compiler: from MIPS to HEX"<<endl;
             cout<<"Version: "<<VERSION<<endl;
         }
     }
@@ -38,9 +85,17 @@ int main(int argc, char const *argv[]){
             cerr<<"Error: Can not open source file "<<argv[0]<<endl;
             return 1;
         }
-        if (!outputFile){
+        else if (!outputFile){
             cerr<<"Error: Can not create output file "<<argv[1]<<endl;
             return 1;
+        }
+        else{
+			vector < string > content;
+			int length = 0;
+			for (; getline(sourceFile, content[length]); length++);
+			for (int i = 0; i < content.size(); i++){
+				trans(content, i);
+			}
         }
     }
     return 0;
