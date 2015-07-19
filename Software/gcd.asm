340e42e8c4be44d8b44848cc9222928f19548c27
Yongwen Zhuang <zyeoman@163.com>
2015-07-19 23:47:09 +0800

    gcd code finish
    add bcd7 showing

--- Software/gcd.asm
+++ Software/gcd.asm
@@ -2,6 +2,7 @@
 # $s0=外设地址指针
 # $s1=数码管译码位置选择
 # $s2=数码管位置
+# $s3=数码管译码指针
 # $t3=TH
 # $t4=TL
 # $t5=TCON
@@ -17,6 +18,8 @@ Error:
     j Error 
 
 init:
+    addi $t0,$zero,20
+    jr $t0
     lui $s0,0x4000
     addi $t3,$zero,-4096
     sw $zero,8($s0) #TCON = 0
@@ -27,6 +30,41 @@ init:
     sll $s2,$t0,4   #$s2=0x10
     sw $t4,4($s0)   #TL=0xFFFFFFFF
     sw $zero,32($s0)#UART_CON=0
+
+# 数码管显示数字
+    addi $s3,$zero,0
+    addi $t0,$zero,0x40
+    sw $t0,0($s3)
+    addi $t0,$zero,0x79
+    sw $t0,4($s3)
+    addi $t0,$zero,0x24
+    sw $t0,8($s3)
+    addi $t0,$zero,0x30
+    sw $t0,12($s3)
+    addi $t0,$zero,0x19
+    sw $t0,16($s3)
+    addi $t0,$zero,0x12
+    sw $t0,20($s3)
+    addi $t0,$zero,0x02
+    sw $t0,24($s3)
+    addi $t0,$zero,0x78
+    sw $t0,28($s3)
+    addi $t0,$zero,0x00
+    sw $t0,32($s3)
+    addi $t0,$zero,0x10
+    sw $t0,36($s3)
+    addi $t0,$zero,0x08
+    sw $t0,40($s3)
+    addi $t0,$zero,0x03
+    sw $t0,44($s3)
+    addi $t0,$zero,0x46
+    sw $t0,48($s3)
+    addi $t0,$zero,0x21
+    sw $t0,52($s3)
+    addi $t0,$zero,0x06
+    sw $t0,56($s3)
+    addi $t0,$zero,0x0E
+    sw $t0,60($s3)
 
 get1:
     lw $t6,32($s0)
@@ -71,7 +109,39 @@ INTERUPT:
     and $t5,$t5,$t8 
     sw $t5,8($s0)       #关闭中断
     #在这里译码数码管显示
-    #译码数码管显示结束
+    beq $s1,$zero,bcd1
+    addi $s6,$s1,-1
+    beq $s6,$zero,bcd2
+    addi $s6,$s6,-1
+    beq $s6,$zero,bcd3
+    addi $s6,$s6,-1
+    beq $s6,$zero,bcd4
+bcd1:
+    and $t0,$t1,0xF
+    sll $t0,$t0,2
+    add $s4,$t0,$s3
+    j endbcd
+bcd2:
+    and $t0,$t1,0xF0
+    srl $t0,$t0,2
+    add $s4,$t0,$s3
+    j endbcd
+bcd3:
+    and $t0,$t2,0xF
+    sll $t0,$t0,2
+    add $s4,$t0,$s3
+    j endbcd
+bcd4:
+    and $t0,$t2,0xF0
+    srl $t0,$t0,2
+    add $s4,$t0,$s3
+    j endbcd
+    #数码管译码结束
+endbcd:
+    lw $s5,0($s4)
+    add $s5,$s5,$s2
+    sw $s5,20($s0)
+    #将译码结果显示
     addi $s1,$s1,1
     addi $t0,$zero,4
     beq $s1,$t0,reset
@@ -83,6 +153,6 @@ reset:
     sll $s2,$t0,4
 exit:
     lw $t5,8($s0)
-    ori $t5,2
+    ori $t5,$t5,2
     sw $t5,8($s0)
     jr $k0
